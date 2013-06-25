b4b1c3f1ac4482be05795b78123dc84cba9dcd2e
jasonblanchard <jas.blanchard@gmail.com>
2013-06-24 21:21:44 -0400

    Parsing page for keywords

--- lib.rb
+++ lib.rb
@@ -1,17 +1,41 @@
 require 'open-uri'
+require 'nokogiri'
 
 class Crawler
     attr_accessor :to_crawl
 
     def initialize(seed)
-        @seed = seed
+        @seed = WebPage.new(seed)
         @to_crawl = []
         @crawled = []
+        @keywords = KeywordRepo.new
     end
 
     def crawl
-        seed_page = WebPage.new(@seed)
-        @to_crawl = @to_crawl + LinkParser.new(seed_page.contents,seed_page.host).get_links
+        start_seed
+    end
+
+    private
+
+    def start_seed
+        @to_crawl = @to_crawl + LinkParser.new(@seed.contents,@seed.host).get_links
+    end
+
+end
+
+class KeywordRepo
+    attr_accessor :keywords
+
+    def initialize
+        @keywords = {}
+    end
+
+    def add_keyword(keyword)
+        @keywords[keyword] = []
+    end
+
+    def add_link_to(keyword, link)
+        @keywords[keyword] << link
     end
 
 end
@@ -54,4 +78,25 @@ class WebPage
         @host = URI.parse(@uri).host
     end
 
+end
+
+class KeywordParser
+    def initialize(uri)
+        @content = Nokogiri::HTML(open('http://www.nytimes.com/'))
+        @parsed_content = parse_content
+    end
+
+
+    def get_keywords
+        @parsed_content.split(' ')
+    end
+
+    private
+    def parse_content
+        get_tag('p')
+    end
+
+    def get_tag(tag)
+        @content.css(tag).map(&:text).join
+    end
 end
--- search.rb
+++ search.rb
@@ -4,7 +4,10 @@ c = Crawler.new('http://kresimirbojcic.com/2011/11/19/dependency-injection-in-ru
 
 c.crawl
 
-c.to_crawl.each do |link|
-    p link
-end
+page = WebPage.new('http://kresimirbojcic.com/2011/11/19/dependency-injection-in-ruby.html')
 
+k = KeywordParser.new(page.contents)
+
+k.get_keywords.each do |k|
+    puts k
+end
